e818aea679067f64229958916d23b949cf772ec0
Levi Chinecherem Chidi <74856946+Levi-Chinecherem@users.noreply.github.com>
2024-01-28 22:24:31 +0100

    updated import dj_database_url

--- main_p/settings.py
+++ main_p/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 import cloudinary
 import cloudinary_storage
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,7 +89,9 @@ DATABASES = {
     }
 }
 
-DATABASES["default"] = dj_database_url.parse("postgres://cloud_image_user:BzvS9M0uQ8RxnRLGFwWp64U8VNatpXYp@dpg-cmrbsef109ks73fhc7gg-a.oregon-postgres.render.com/cloud_image_db")
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
+# DATABASES["default"] = dj_database_url.parse("postgres://cloud_image_user:BzvS9M0uQ8RxnRLGFwWp64U8VNatpXYp@dpg-cmrbsef109ks73fhc7gg-a.oregon-postgres.render.com/cloud_image_db")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
